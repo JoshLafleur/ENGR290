3c4e68715d80ee79a627d5e2663933bb0f444631
Joshua Lafleur <77811114+JoshLafleur@users.noreply.github.com>
2023-02-01 18:56:08 -0500

    added library locally and working code

--- tech1/tech1.ino
+++ tech1/tech1.ino
@@ -21,7 +21,7 @@
 /*SHARP GP2Y0A21YK0F IR distance sensor with Arduino and SharpIR library example code. More info: https://www.makerguides.com */
 
 // Include the library:
-#include <SharpIR.h>
+#include "SharpIR.h"
 
 // Define model and input pin:
 #define IRPin A0
@@ -43,12 +43,18 @@ SharpIR mySensor = SharpIR(IRPin, model);
 void setup() {
   // Begin serial communication at a baudrate of 9600:
   Serial.begin(9600);
-  // pinMode(D11, OUTPUT); // D11 = PB3 = D3 
+  pinMode(11, OUTPUT); // D11 = PB3 = D3 
 }
 
 void loop() {
   // Get a distance measurement and store it as distance_cm:
   distance_cm = mySensor.distance();
+
+  static uint8_t tmp = 0;
+
+  tmp += 50;
+
+  analogWrite(11, tmp);
 
   // Print the measured distance to the serial monitor:
   Serial.print("Mean distance: ");
