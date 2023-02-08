0184fdb5ac54c55a2c48def1f43e6074bd91b69d
Kirill <petergranitski@gmail.com>
2023-02-08 18:48:20 -0500

    Revert "[#1] Partial baremetalification of Serial print statements"
    This reverts commit 85648facc8b9faeca38860b05f7d02b4fcc5369e.
    
    These baremetal USART changes are getting in the way of other stuff.
    Possibly revisit this after other more critical portions are completed.

--- tech1/tech1.ino
+++ tech1/tech1.ino
@@ -6,20 +6,8 @@
 #define IRPin A0
 #define model 1080
 
-// Compiler directives to compute UBBR
-#define F_CPU 16000000 // F_CPU needs to be declared BEFORE including delay.h
-#define BAUD 9600
-#define MYUBBR F_CPU/16/BAUD-1 // UBBR --> Usart Baud Rate register
-
-// even though arduino IDE imports these automatically, just in case we build this in a different environment
-// we still want our register names to resolve properly
-#include <avr/io.h>
-#include <stdio.h>
-
 // Create variable to store the distance:
 int distance_cm;
-// and to print that value
-char* s_distance_cm;
 
 /* Model :
   GP2Y0A02YK0F --> 20150
@@ -32,10 +20,9 @@ char* s_distance_cm;
 SharpIR mySensor = SharpIR(IRPin, model);
 UltraSonicDistanceSensor distanceSensor(12, 8);  // Initialize sensor that uses digital pins 13 and 12.
 
-
 void setup() {
   // Begin serial communication at a baudrate of 9600:
-  USART_init(MYUBBR);
+  Serial.begin(9600);
   pinMode(11, OUTPUT); // D11 = PB3 = D3 
 }
 
@@ -45,46 +32,8 @@ void loop() {
   distance_cm = distanceSensor.measureDistanceCm();
   
   // Print the measured distance to the serial monitor:
-  // Serial.print("Mean distance: ");
-  // Serial.print(distance_cm);
-  msg_transmit("Mean distance: ", 15);
-
-  //itoa(distance_cm, s_distance_cm, 10);
-  // msg_transmit(s_distance_cm, 3); // numerical values maximum of 3 digits
-  Serial.print(distance_cm, DEC);
-  // msg_transmit("cm\n", 3); //why does this act so flaky?? sometimes prints m, sometimes cm... hmmm
-  Serial.println(" cm");
-
-  /*
-                                              UART DEBUGGING ATTEMPTS START
-  */
-  // if (distance_cm >= 100) {
-  //   msg_transmit(" cm\n", 4);
-  // } else {
-  //   msg_transmit("  cm\n", 5);
-  // }
-
-  // why does using iota and printing the value block everything ??????
-  // char* s;
-  // itoa(distance_cm, s, 10);
-  
-  // char buffer[32];
-
-  // for (int i = 0; i < strlen(s); i++) {
-  //   buffer[i] = s[i];
-  // }
-
-  // Serial.print(buffer);
-  //Serial.println(" cm");
-
-
-  //msg_transmit(s, 4); // will only be 3 digits max;
-  //Serial.println(typeid(distance_cm).name())
-  //Serial.print(distance_cm);
-
-    /*
-                                              UART DEBUGGING ATTEMPTS END
-  */
+  Serial.print("Mean distance: ");
+  Serial.print(distance_cm);
 
   if (distance_cm <= 15) {
     analogWrite(11, 0);
@@ -94,34 +43,7 @@ void loop() {
   else {
     analogWrite(11, 255*((float) (distance_cm - 15)/30));
   }
+  Serial.println(" cm");
 
   delay(50);
-}
-
-
-void USART_init(unsigned int ubrr) {
-    // Set baud rate
-    UBRR0H = (unsigned char)(ubrr >> 8); // Shift needed to clear out first 8 bits
-    UBRR0L = (unsigned char) (ubrr);
-
-    // Enable receiver and transmitter
-    UCSR0B = ((1 << RXEN0) | (1 << TXEN0));
-
-    // Set two stop bits with USBS0, set frame to 8 bits of data with UCSz00
-    UCSR0C = ((1 << USBS0) | (3 << UCSZ00));
-}
-
-void USART_transmit(char data) {
-    // Wait for empty transmit buffer
-    while (!(UCSR0A & (1 << UDRE0)));
-
-    // Place data into buffer and send
-    UDR0 = data;
-}
-
-void msg_transmit(char* data, int len) {
-    for (int i = 0; i < len; i++) {
-        USART_transmit(data[i]);
-        // _delay_us(10);
-    }
 }
