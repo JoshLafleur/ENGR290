9b37a4179a141acaffeadd87874dc30f3fc49e59
Kirill <petergranitski@gmail.com>
2023-02-08 20:33:57 -0500

    [#1] Setup sensor mode selection flow on startup
    0 --> IR
    1 --> US
    else --> reset arduino

--- tech1/tech1.ino
+++ tech1/tech1.ino
@@ -9,6 +9,18 @@
 // Create variable to store the distance:
 int distance_cm;
 
+// global status vars
+// MODE 0 --> IR sensor
+// MODE 1 --> US sensor
+int mode = -1;
+
+// US SENSOR SPECIFIC
+
+// IR SENSOR SPECIFIC
+int trigPin = 14;//Trig – green Jumper 
+int echoPin = 5;    //Echo – yellow Jumper
+long duration, cm, inches;
+
 /* Model :
   GP2Y0A02YK0F --> 20150
   GP2Y0A21YK0F --> 1080
@@ -20,17 +32,72 @@ int distance_cm;
 SharpIR mySensor = SharpIR(IRPin, model);
 UltraSonicDistanceSensor distanceSensor(12, 8);  // Initialize sensor that uses digital pins 13 and 12.
 
+// function to reset arduino
+void(* resetFunc) (void) = 0; // declare reset function @ address 0
+
+
+int selectMode() {
+  Serial.println("Which mode would you like to test?");
+  Serial.println("OPTIONS:");
+  Serial.println("0 --> IR sensor");
+  Serial.println("1 --> US sensor");
+
+  // Wait for user input
+  while (Serial.available() == 0);
+
+  int option = Serial.parseInt();
+
+  if (option == 0) {
+    Serial.println("You chose: option 0 --> IR sensor");
+    return option;
+  } else if (option == 1) {
+    Serial.println("You chose: option 1 - US sensor");
+    return option;
+  } else {
+    Serial.println("Invalid option, try again");
+    delay(100);
+    resetFunc();
+  }
+}
+
+// the calm before the storm...
+void pauseChamp() {
+  Serial.print("Starting in 3 seconds.");
+  delay(1000);
+  Serial.print(".");
+  delay(1000);
+  Serial.println(".");
+  delay(1000);
+}
+
 void setup() {
   // Begin serial communication at a baudrate of 9600:
   Serial.begin(9600);
-  pinMode(11, OUTPUT); // D11 = PB3 = D3 
+
+  mode = selectMode();
+  pauseChamp();
+
+  if (mode == 0) { // IR SENSOR
+  
+  } else if (mode == 1) { // US SENSOR
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+  }
+  
+  pinMode(11, OUTPUT); // D11 = PB3 = D3
 }
 
 void loop() {
   // Get a distance measurement and store it as distance_cm:
   //distance_cm = mySensor.distance();
-  distance_cm = distanceSensor.measureDistanceCm();
-  
+
+  if (mode == 0) { // IR SENSOR
+    distance_cm = mySensor.distance();    
+  }
+  else if (mode == 1) { // US SENSOR
+    distance_cm = distanceSensor.measureDistanceCm();
+  }
+    
   // Print the measured distance to the serial monitor:
   Serial.print("Mean distance: ");
   Serial.print(distance_cm);
