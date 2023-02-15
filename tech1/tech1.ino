09c1ff54373ca18b132de5a175b881ceeaecffd3
Kirill <petergranitski@gmail.com>
2023-02-15 16:13:08 -0500

    [#1] Port all digital writes to baremetal - though issue with D3 control

--- tech1/tech1.ino
+++ tech1/tech1.ino
@@ -21,14 +21,7 @@ int trigPin = 14;   //Trig – green Jumper // 14 --> PB0
 int echoPin = 5;    //Echo – yellow Jumper // 5 --> PD3
 long duration, cm, inches;
 
-/* Model :
-  GP2Y0A02YK0F --> 20150
-  GP2Y0A21YK0F --> 1080
-  GP2Y0A710K0F --> 100500
-  GP2YA41SK0F --> 430
-*/
-
-// Create a new instance of the SharpIR class:
+// We are using model GP2Y0A21YK0F i.e. 1080
 SharpIR mySensor = SharpIR(IRPin, model);
 UltraSonicDistanceSensor distanceSensor(12, 8);  // Initialize sensor that uses digital pins 13 and 12.
 
@@ -84,7 +77,7 @@ void setup() {
     //pinMode(trigPin, OUTPUT); // 14 --> PB0
     DDRB &= ~(1 << 0);
     //pinMode(echoPin, INPUT);  // 5  --> PD3
-    DDRD |= (1 << 3);
+    DDRD |= (1 << 3); 
   }
 
   // pinMode(11, OUTPUT); // D11 = PB3 = D3
@@ -96,7 +89,7 @@ void loop() {
   //distance_cm = mySensor.distance();
 
   if (mode == 0) { // IR SENSOR
-    distance_cm = mySensor.distance();    
+    distance_cm = mySensor.distance();   
   }
   else if (mode == 1) { // US SENSOR
     distance_cm = distanceSensor.measureDistanceCm();
@@ -106,15 +99,29 @@ void loop() {
   Serial.print("Mean distance: ");
   Serial.print(distance_cm);
 
+  // spec: "15cm or less - 100%""
   if (distance_cm <= 15) {
-    analogWrite(11, 0);
-    digitalWrite(13,LOW);
-  } else if (distance_cm > 45) {
-    analogWrite(11, 255);
-    digitalWrite(13,HIGH);
+    //analogWrite(11, 0);
+    //digitalWrite(11, HIGH);
+    PORTB |= (1 << 3); // @TODO: is this not working properly? i.e. why does LED D3 not seem to budge....
+
+    //digitalWrite(13, HIGH); // pin 13 = PB5
+    PORTB |= (1 << 5);
+
+  }
+  // spec: "45cm or more - 0%"
+  else if (distance_cm > 45) {
+    //analogWrite(11, 255);
+    //digitalWrite(11, LOW); // pin 11 = PB3
+    PORTB &= (1 << 3); // @TODO: is this not working properly? i.e. why does LED D3 not seem to budge...
+    //digitalWrite(13,HIGH); // pin 13 = PB5
+    PORTB |= (1 << 5);
   }
   else {
-    analogWrite(11, 255*((float) (distance_cm - 15)/30));
+    // spec: "linearly increases from 45 cm to 15 cm"
+    analogWrite(11, (255*((float) (distance_cm - 15)/30)));
+    //digitalWrite(13, LOW); // pin 13 = PB5
+    PORTB &= ~(1 << 5);
   }
   Serial.println(" cm");
 
